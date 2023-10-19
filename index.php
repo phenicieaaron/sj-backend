4d842a37a9c1d6d55dd0aa9d6ecabba7d2824f6d
phenicieaaron <phenicieaaron@yahoo.com>
2023-10-18 20:01:09 -0500

    resolves iss1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <hr/>
+       
+        <br>
+        <a href='admin_menu.php'>Profile</a>
+        <a href='login.php'>Login</a>
+    </body>
+</html>
