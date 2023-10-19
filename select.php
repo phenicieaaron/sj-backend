50559a23bc94b0b3bb09b4675cc57f5ac3e8aedb
phenicieaaron <phenicieaaron@yahoo.com>
2023-10-18 20:07:21 -0500

    resolves iss3

---
+++ select.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+<h2>reading from the database and returning to console log:</h2>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "recipes";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM items";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+               echo " - Dish: " . $row['dish'] . "<br>"." - Protein: " . $row["protein"] . "<br>" . " - Difficulty: " . $row['difficulty'] . "<br>" . " - Link: " . $row["link"] . "<br><br>";
+               $data = $row;
+               echo "<script>console.log('" . json_encode($data) . "' );</script>";
+
+            
+                
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
+
+        <br>
+        <a href="admin_menu.php">Return to profile</a>
+    </body>
+</html>
